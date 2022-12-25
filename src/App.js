55308fab0f88ede14916b9a256056b71e048022c
majid <bimantara1104@gmail.com>
2022-12-25 22:02:55 +0700

    adjustment main apps

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import { useDispatch, useSelector } from 'react-redux';
+import TestApi from './pages/TestApi';
+import LoginPage from './pages/LoginPage';
+import Loading from './components/Loading';
+import RegisterPage from './pages/RegisterPage';
+import { asyncPreloadProcess } from './states/isPreload/action';
 
 function App() {
+  const {
+    authUser = null,
+    isPreload = false,
+  } = useSelector((states) => states);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(asyncPreloadProcess());
+  }, [dispatch]);
+
+  console.log(authUser, isPreload);
+
   return (
     <>
-      <header>
-        {/* navigate */}
-      </header>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <Loading />
+      <main>
+        <Routes>
+          <Route path="/*" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/api-test" element={<TestApi />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 // import { Provider } from 'react-redux';
 import App from './App';
+import store from './states';
+
+import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
-// <Provider>
-// </Provider>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>,
 );
