b8702afe97a7f0bd02cbc84536a43d98f6e77fd0
majid <bimantara1104@gmail.com>
2022-12-27 00:58:26 +0700

    button add component

---
+++ src/components/ButtonAdd.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  IoAddCircle,
+} from 'react-icons/io5';
+
+function ButtonAdd() {
+  return (
+    <button type="button" className="new-thread-button">
+      <IoAddCircle />
+    </button>
+  );
+}
+
+export default ButtonAdd;
