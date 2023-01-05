e01315b0dc14614a88952a003939cd898eb8b7be
majid <bimantara1104@gmail.com>
2023-01-06 00:40:21 +0700

    refactor navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import {
-  IoHome, IoLogOutOutline, IoBarChart,
-} from 'react-icons/io5';
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Input,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+} from 'reactstrap';
+import { FaRegUser, FaSignOutAlt } from 'react-icons/fa';
 
 function Navigation({ authUser, signOut }) {
   const { id, avatar, name } = authUser;
 
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
   return (
-    <div className="navigation">
-      <nav>
-        <Link to="/"><IoHome size={30} /></Link>
-        <Link to="/leaderboards"><IoBarChart size={30} /></Link>
-      </nav>
-      <Link to="/me">
-        <img src={avatar} alt={id} title={name} />
-      </Link>
-      <button type="button" onClick={signOut}>
-        <IoLogOutOutline size={30} />
-      </button>
-    </div>
+    <Navbar fixed="top" className="p-0 shadow-sm" color="white">
+      <NavbarBrand>
+        <Link to="/">
+          <img src="/logo.png" alt="brand_logo" height={30} />
+        </Link>
+      </NavbarBrand>
+      <NavbarText>
+        <Input type="search" className="search-input rounded-pill bg-light" color="light" placeholder="Search Discussion" />
+      </NavbarText>
+      <NavbarText>
+        <Dropdown toggle={toggle} isOpen={dropdownOpen} color="link">
+          <DropdownToggle color="link" className="text-decoration-none text-capitalize">
+            {name}
+            <img src={avatar} alt={id} title={name} className="rounded-circle fa ms-1" height={30} />
+          </DropdownToggle>
+          <DropdownMenu className="border shadow">
+            <DropdownItem>
+              <Link to="/me" className="text-decoration-none text-capitalize">
+                <FaRegUser className="me-1" />
+                Profile
+              </Link>
+            </DropdownItem>
+            <DropdownItem onClick={signOut}>
+              <FaSignOutAlt className="me-1" />
+              Logout
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarText>
+    </Navbar>
   );
 }
 
