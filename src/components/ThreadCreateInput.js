b1206ee5bb6d275ca99e5be91924a587923685ee
majid <bimantara1104@gmail.com>
2023-01-06 01:17:10 +0700

    refactor createPage

--- src/components/ThreadCreateInput.js
+++ src/components/ThreadCreateInput.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Input,
+} from 'reactstrap';
 
 function ThreadCreateInput({
   createThread,
@@ -8,7 +14,7 @@ function ThreadCreateInput({
   const [category, setCategory] = useState('');
   const [body, setBody] = useState('');
 
-  function createThreadHandler(e) {
+  const createThreadHandler = (e) => {
     e.preventDefault();
     if (body.trim() && title.trim() && category.trim()) {
       createThread({ title, body, category });
@@ -16,54 +22,58 @@ function ThreadCreateInput({
       setCategory('');
       setBody('');
     }
-  }
+  };
 
-  function handleTitleChange({ target }) {
+  const handleTitleChange = ({ target }) => {
     setTitle(target.value);
-  }
+  };
 
-  function handleCategoryChange({ target }) {
+  const handleCategoryChange = ({ target }) => {
     setCategory(target.value);
-  }
+  };
 
-  function handleBodyChange({ target }) {
+  const handleBodyChange = ({ target }) => {
     if (target.value.length <= 1000) {
       setBody(target.value);
     }
-  }
+  };
 
   return (
-    <form onSubmit={createThreadHandler}>
-      <div className="thread-input">
-        <input
+    <Form onSubmit={createThreadHandler}>
+      <FormGroup>
+        <Input
           type="text"
           placeholder="Title"
           value={title}
           onChange={handleTitleChange}
           required
         />
-        <input
+      </FormGroup>
+      <FormGroup>
+        <Input
           type="text"
           placeholder="Category"
           value={category}
           onChange={handleCategoryChange}
           required
         />
-        <textarea
-          data-cy="thread-input"
-          type="text"
+      </FormGroup>
+      <FormGroup>
+        <Input
+          type="textarea"
+          rows="5"
           placeholder="Type something...."
           value={body}
           onChange={handleBodyChange}
           required
         />
-        <p className="thread-input__char-left">
-          <strong>0</strong>
+        <small>
+          <strong>{body.length}</strong>
           /2000
-        </p>
-        <button data-cy="thread-button" type="submit">Talk</button>
-      </div>
-    </form>
+        </small>
+      </FormGroup>
+      <Button block type="submit" color="primary">Save</Button>
+    </Form>
   );
 }
 
